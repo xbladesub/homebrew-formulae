aa8e1c4d6c19fdc66f1e5aedce48f273946dae14
Nikolai Shelekhov <nickshv13@gmail.com>
2021-05-27 16:38:12 +0800

    Update stox.rb

--- Formula/stox.rb
+++ Formula/stox.rb
@@ -1,5 +1,5 @@
 class Stox < Formula
-  desc "A Swift command-line tool to view and export tickers from finviz.com stock screener."
+  desc "Swift command-line tool to view and export tickers from finviz.com"
   homepage "https://github.com/xbladesub/stox"
   url "https://github.com/xbladesub/stox/archive/1.0.1.tar.gz"
   sha256 "f037220a0b810775dad6e4df77f335aa9e96ed10b94784ae8d2821a69c628e3b"
