595020670ecdaa4e0087b30c3b0318e5862b3973
Nikolai Shelekhov <nickshv13@gmail.com>
2021-05-27 21:13:18 +0800

    Update stox.rb

--- Formula/stox.rb
+++ Formula/stox.rb
@@ -4,18 +4,19 @@ class Stox < Formula
   url "https://github.com/xbladesub/stox/archive/1.0.1.tar.gz"
   sha256 "f037220a0b810775dad6e4df77f335aa9e96ed10b94784ae8d2821a69c628e3b"
   license "MIT"
-
+  
   depends_on xcode: "12.4"
-
+  
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", \
-      "-cross-module-optimization", "--enable-test-discovery"
-    mv ".build/release/stox", "stox"
-    bin.install "stox"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc",
+                    "-cross-module-optimization", "--enable-test-discovery"
+    bin.install ".build/release/stox"
   end
-
+  
   test do
-      output = shell_output("#{bin}/stox all")
-      assert_match("\nNo saved lists\n", output)
+      (testpath/"test.swift").write <<~EOS
+        import Foundation
+      EOS
+      assert_predicate testpath/"test.swift", :exist?
   end
 end
