814feda6919d75a7f8af33ff044ab627a9308460
Nikolai Shelekhov <nickshv13@gmail.com>
2021-05-27 16:15:50 +0800

    Update stox.rb

--- Formula/stox.rb
+++ Formula/stox.rb
@@ -1,19 +1,21 @@
 class Stox < Formula
-  desc "Swift command-line tool to view and export tickers from finviz.com"
+  desc "A Swift command-line tool to view and export tickers from finviz.com stock screener."
   homepage "https://github.com/xbladesub/stox"
   url "https://github.com/xbladesub/stox/archive/1.0.1.tar.gz"
   sha256 "f037220a0b810775dad6e4df77f335aa9e96ed10b94784ae8d2821a69c628e3b"
   license "MIT"
-  
+
   depends_on xcode: "12.4"
-  
+
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc",
-                    "-cross-module-optimization", "--enable-test-discovery"
-    bin.install ".build/release/stox"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", \
+      "-cross-module-optimization", "--enable-test-discovery"
+    mv ".build/release/stox", "stox"
+    bin.install "stox"
   end
-  
+
   test do
-    system bin/"stox", "all"
+#      system bin/"stox", "--help"
+      assert_equal "Delete all lists? (y / n): ", del
   end
 end
