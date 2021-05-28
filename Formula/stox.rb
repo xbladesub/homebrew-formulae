4d083bd76ce630ad55038fb250c4a73616a45e82
Nikolai Shelekhov <nickshv13@gmail.com>
2021-05-28 09:42:56 +0800

    Update stox.rb

--- Formula/stox.rb
+++ Formula/stox.rb
@@ -4,15 +4,15 @@ class Stox < Formula
   url "https://github.com/xbladesub/stox/archive/1.0.1.tar.gz"
   sha256 "f037220a0b810775dad6e4df77f335aa9e96ed10b94784ae8d2821a69c628e3b"
   license "MIT"
-  
+
   depends_on xcode: "12.4"
-    
+
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc",
            "-cross-module-optimization", "--enable-test-discovery"
     bin.install ".build/release/stox"
   end
-  
+
   test do
     (testpath/"test.swift").write <<~EOS
       import Foundation
